0bc0e0e88de2da7e265c71d8131cfb5296beb1c8
Zeken <sahilpatel3205@gmail.com>
2023-07-16 13:22:08 +0530

    infinite scroll added and bugs fixed

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import News from './components/News';
 import './App.css';
 
 export default function App() {
+  let pageSize = 15;
   return (
     <div>
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route exact path="/" element={<News key="general" pagesize={6} country="in" category="general" />} />
-          <Route exact path="/Business" element={<News key="business" pagesize={6} country="in" category="business" />} />
-          <Route exact path="/Entertainment" element={<News key="entertainment" pagesize={6} country="in" category="entertainment" />} />
-          <Route exact path="/Health" element={<News key="health" pagesize={6} country="in" category="health" />} />
-          <Route exact path="/Science" element={<News key="science" pagesize={6} country="in" category="science" />} />
-          <Route exact path="/Technology" element={<News key="technology" pagesize={6} country="in" category="technology" />} />
+          <Route exact path="/" element={<News key="general" pagesize={pageSize} country="in" category="general" />} />
+          <Route exact path="/Business" element={<News key="business" pagesize={pageSize} country="in" category="business" />} />
+          <Route exact path="/Entertainment" element={<News key="entertainment" pagesize={pageSize} country="in" category="entertainment" />} />
+          <Route exact path="/Health" element={<News key="health" pagesize={pageSize} country="in" category="health" />} />
+          <Route exact path="/Science" element={<News key="science" pagesize={pageSize} country="in" category="science" />} />
+          <Route exact path="/Technology" element={<News key="technology" pagesize={pageSize} country="in" category="technology" />} />
         </Routes>
       </BrowserRouter>
     </div>
